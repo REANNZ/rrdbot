5b8f4f1a6cc9fbf525caa385873a0a668705c3cc
Stef Walter <stef@memberwebs.com>
2009-10-28 23:51:22 +0000

    Setup git hooks properly when a new checkout

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+# Some boiler plate to get git setup as expected
+if test -d .git; then
+	if test -f .git/hooks/pre-commit.sample && \
+	   test ! -f .git/hooks/pre-commit; then
+		cp -pv .git/hooks/pre-commit.sample .git/hooks/pre-commit
+	fi
+fi
+
 aclocal
 autoheader
 automake -a
